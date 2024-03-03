773cf82ce88fb6eee780125b63077915a8612a85
deniachmad <deni.achmad@m3.formulatrix.com>
2024-03-03 07:56:19 +0700

    add link for contact button

--- src/routes/component/home/btnSosmed.jsx
+++ src/routes/component/home/btnSosmed.jsx
@@ -1,15 +1,31 @@
 import { Icon } from '@iconify/react';
 export default function BtnSosmed() {
-    return (
-        <div className=" flex justify-center">
-              <div className="w-fit grid grid-cols-5 gap-2 xl:gap-6">
-                <Icon className="lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500" icon="mdi-gmail" />
-                <Icon className="lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500" icon="ant-design:github-filled" />
-                <Icon className="lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500" icon="mingcute:whatsapp-line" />
-                <Icon className="lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500" icon="mdi-instagram" />
-                <Icon className="lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500" icon="mingcute:linkedin-fill" />
+  const tailwind = "lg:w-10 lg:h-10 xl:w-12 xl:h-12 border border-neutral-400 p-2 rounded-lg text-neutral-300 hover:text-white hover:border-white hover:scale-150 hover:duration-[400ms] hover:bg-zinc-500"
+  const openGmail = () => {
+    window.open('https://mail.google.com/mail/u/0/?fs=1&to=im.deniachmad@gmail.com&tf=cm', '_blank');
+  };
+  const openGit = () => {
+    window.open('https://github.com/imdeni', '_blank');
+  };
+  const openWa = () => {
+    window.open('https://api.whatsapp.com/send?phone=6285803619114', '_blank');
+  };
+  const openIg = () => {
+    window.open('https://www.instagram.com/dee__721/', '_blank');
+  };
+  const openLinkedin = () => {
+    window.open('https://linkedin.com/in/deni-achmad-813938289', '_blank');
+  };
+  return (
+    <div className=" flex justify-center">
+      <div className="w-fit grid grid-cols-5 gap-2 xl:gap-6">
+        <Icon onClick={openGmail} className={tailwind} icon="mdi-gmail" />
+        <Icon onClick={openGit} className={tailwind} icon="ant-design:github-filled" />
+        <Icon onClick={openWa} className={tailwind} icon="mingcute:whatsapp-line" />
+        <Icon onClick={openIg} className={tailwind} icon="mdi-instagram" />
+        <Icon onClick={openLinkedin} className={tailwind} icon="mingcute:linkedin-fill" />
 
-              </div>
-            </div>
-    );
+      </div>
+    </div>
+  );
 }
